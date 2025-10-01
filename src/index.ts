a77a05202f06a087f12bd7881c1fc91116f1721b
smeet96 <smeetsherki@gmail.com>
2025-10-01 20:58:05 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import jwt from "jsonwebtoken"
 import userRouter from "./Routes/userRoute"
 import contentRouter from "./Routes/contentRoute"
 import dotenv from "dotenv"
+import cors from "cors"
 const app = express()
 app.use(express.json())
 dotenv.config()
 const pass = process.env.pass!
+
+app.use(cors());
 
 declare global {
   namespace Express {
